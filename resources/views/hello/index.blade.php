e51eee9f8d689b42b328cdc606a1b7a44c87ff72
JUN <soipon0@gmail.com>
2019-04-25 18:48:53 +0900

    @componentを使ってコンポーネントを組み込む

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,7 +9,18 @@
 
 @section('content')
     <p>ここが本文のコンテンツです</p>
-    <p>必要な記述ができます</p>
+    <p>必要なだけ記述ができます</p>
+    
+    @component('components.message')
+        @slot('msg_title')
+        CAUTION!
+        @endslot
+
+        @slot('msg_content')
+        これはメッセージの表示です。
+        @endslot
+    @endcomponent
+    
 @endsection
 
 @section('footer')
