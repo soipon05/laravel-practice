4db41025156a9437511da9f69265da9419ca1198
JUN <soipon0@gmail.com>
2019-04-26 23:25:22 +0900

    formバリデーション

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,9 +8,17 @@
 @endsection
 
 @section('content')
-    <p>ここが本文のコンテンツです</p>
-    <p>これは、<middleware>google.com</middleware>へのリンクです。</p>
-    <p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです。</p>
+    <p>{{$msg}}</p>
+    <table>
+        <form action="/hello" method="post">
+            {{ csrf_field() }}
+            <tr><th>name: </th><td><input type="text" name="name"></td></tr>
+            <tr><th>email: </th><td><input type="text" name="email"></td></tr>
+            <tr><th>age: </th><td><input type="text" name="age"></td></tr>
+            <tr><th></th><td><input type="sumit" value="send"></td></tr>
+
+        </form>
+    </table>
 @endsection
 
 @section('footer')
