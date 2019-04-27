3b2d80a381b968712cc64ff9e6f874f62568e848
JUN <soipon0@gmail.com>
2019-04-27 17:08:46 +0900

    formrequestを使ったバリデーション

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
 
 class HelloController extends Controller
 {
@@ -49,14 +50,8 @@ public function index(Request $request)
         return view('hello.index', ['msg'=>'フォームを入力：']);
     }
 
-public function post(Request $request)
+public function post(HelloRequest $request)
     {
-        $validate_rule = [
-            'name' =>   'required',
-            'mail' =>   'email',
-            'age'  =>   'numeric|between:0,150',
-        ];
-        $this->validate($request, $validate_rule);
         return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 
