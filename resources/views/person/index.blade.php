83e98db214b6915b9efc7d7a588f4fea663e7740
JUN <soipon0@gmail.com>
2019-04-29 18:40:17 +0900

    findを使ってインスタンを特定

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -8,17 +8,14 @@
 @endsection
 
 @section('content')
+<p>ID: name(age)</p>
     <table>
         <tr>
-            <th>Name</th>
-            <th>Mail</th>
-            <th>Age</th>
+            <th>Data</th>
         </tr>
         @foreach ($items as $item)
             <tr>
-                <td>{{ $item->name }}</td>
-                <td>{{ $item->mail }}</td>
-                <td>{{ $item->age }}</td>
+                <td>{{ $item->getData() }}</td>
             </tr>
         @endforeach
     </table>
--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,7 @@ Route::get('hello/show',    'HelloController@show');
 // Person
 // 
 
-Route::get('person', 'PersonController@index');
+Route::get('person',        'PersonController@index');
+
+Route::get('person/find',   'PersonController@find');
+Route::post('person/find',  'PersonController@search');
