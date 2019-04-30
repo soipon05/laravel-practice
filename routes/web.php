6b6096c38f954d7db99029093801fb1997850c83
JUN <soipon0@gmail.com>
2019-04-30 18:19:27 +0900

    ログイン画面の作成

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,9 @@ Route::get('/test', function () {
 
 // Route::get('hello',         'HelloController@index');
 // Route::get('hello/{id?}', 'HelloController@index');
-Route::post('hello',            'HelloController@post');
+// Route::post('hello',            'HelloController@post');
 Route::get('hello',             'HelloController@index')
-    ->middleware('helo');   
+    ->middleware('auth');   
 // Peopleの新規作成 
 Route::get('hello/add',         'HelloController@add');
 Route::post('hello/add',        'HelloController@create');
@@ -33,11 +33,14 @@ Route::get('hello/del',         'HelloController@del');
 Route::post('hello/del',        'HelloController@remove');
 // Peopleの詳細画面 
 Route::get('hello/show',        'HelloController@show');
-    
+
 Route::get('hello/rest',        'HelloController@rest');
-    
+
 Route::get('hello/session',     'HelloController@ses_get');
 Route::post('hello/session',    'HelloController@ses_put');
+
+Route::get('hello/auth',        'HelloController@getAuth');
+Route::post('hello/auth',       'HelloController@postAuth');
 
 // Person
 // 
