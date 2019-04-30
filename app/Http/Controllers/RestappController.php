763d0ecc9e4d339d1e7b129a8597d61540596419
JUN <soipon0@gmail.com>
2019-04-30 13:59:07 +0900

    createとstoreの処理追加

--- app/Http/Controllers/RestappController.php
+++ app/Http/Controllers/RestappController.php
@@ -25,7 +25,7 @@ class RestappController extends Controller
      */
     public function create()
     {
-        //
+        return view('rest.create');
     }
 
     /**
@@ -36,7 +36,11 @@ class RestappController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $restdata = new Restdata;
+        $form     = $request->all();
+        unset($form['_token']);
+        $restdata->fill($form)->save();
+        return redirect('/rest');
     }
 
     /**
@@ -48,7 +52,7 @@ class RestappController extends Controller
     public function show($id)
     {
         $item = Restdata::find($id);
-        return $item->toArray;
+        return $item->toArray();
     }
 
     /**
